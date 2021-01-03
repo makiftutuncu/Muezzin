554d4059ac94b7096542ce4ef25c90fddde6eea8
Besir Turkoglu <engi2nee@gmail.com>
2021-01-03 18:59:05 +0300

    fix default text color showing as white

--- app/src/main/java/com/mehmetakiftutuncu/muezzin/extension/Context.kt
+++ app/src/main/java/com/mehmetakiftutuncu/muezzin/extension/Context.kt
@@ -1,11 +1,14 @@
 package com.mehmetakiftutuncu.muezzin.extension
 
 import android.content.Context
-import android.util.TypedValue
+import android.graphics.Color
 import androidx.annotation.AttrRes
+import androidx.core.content.res.use
 
 fun Context.themeColor(@AttrRes attrRes: Int): Int {
-    val typedValue = TypedValue()
-    theme.resolveAttribute (attrRes, typedValue, true)
-    return typedValue.data
+    return obtainStyledAttributes(
+        intArrayOf(attrRes)
+    ).use {
+        it.getColor(0, Color.BLACK)
+    }
 }
